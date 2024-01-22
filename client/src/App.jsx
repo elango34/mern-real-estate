04cd761e0e4b77f91628e7ac27fa6becb4f84587
elango <elango@cloudnix.com>
2024-01-22 21:09:21 +0530

    header added and initialised api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import React from 'react'
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Home() {
+  console.log("home");
+  return (
+    <h3>Home</h3>
+  )
+}
