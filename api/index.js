2a1af382e97a862805e67c3ba353950c30749680
elango <elango@cloudnix.com>
2024-01-31 20:10:54 +0530

    created test route with mvc structure and tested it

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+// 
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,8 +13,10 @@ mongoose.connect(process.env.MONGODB_CONNECT).then(()=>{
     console.log("mongo db connected");
 }).catch((err)=>{
     console.log(err);
-})
+});
 
-app.listen(3000, ()=> {
-    console.log("listening on port 3000..");
+app.use("/api/user",userRouter);
+
+app.listen(3001, ()=> {
+    console.log("listening on port 3001..");
 })
