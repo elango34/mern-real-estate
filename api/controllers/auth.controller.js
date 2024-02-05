6f5d443eb3b3caec8005fa9bfe410d4917cde570
elango <elango@cloudnix.com>
2024-02-05 21:53:07 +0530

    completed signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,11 @@ export const signup = async(req, res, next) => {
         const hashedPassword = bcryptjs.hashSync(password, 10);
         const user = new User({userName, password: hashedPassword, email});
         await user.save();
-        res.status(201).json("successfully saved");
+        res.status(201).json({
+            success: true,
+            statusCode: 201,
+            message: "successfully saved"
+        })
     } catch(error) {
         // https://www.notion.so/Node-js-a74a450ee2864a04b641e826d88e956d?pvs=4#d24f3242fa0a4bc6b162e024581b3146
         // Instead of sending error from each controller, use error middleware from all place
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formEntries, setFormEntries] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState("");
+  const navigate = useNavigate();
+
+  // console.log(formEntries);
+  const inputChangeHandler = (event) => {
+    setFormEntries({
+      ...formEntries,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const submitHandler = async (event) => {
+    console.log("submit");
+    event.preventDefault();
+    setIsLoading(true);
+    // send it to backend
+    try {
+      const rawResponse = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formEntries),
+      });
+      const response = await rawResponse.json();
+
+      setIsLoading(false);
+      console.log(response);
+
+      if (!response.success) {
+        throw new Error(response.message);
+      }
+      navigate("/sign-in");
+    } catch (error) {
+      console.log("some error occurred");
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          name="userName"
+          value={formEntries.userName}
+          onChange={inputChangeHandler}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          name="email"
+          value={formEntries.email}
+          onChange={inputChangeHandler}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          name="password"
+          value={formEntries.password}
+          onChange={inputChangeHandler}
+        />
+        <button
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? "Loading..." : "Sign up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
